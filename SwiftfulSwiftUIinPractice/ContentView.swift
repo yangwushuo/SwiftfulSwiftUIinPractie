8e988c83f51c53f720b6298d0648efa40f66275d
jasonyang <51365593+yangwushuo@users.noreply.github.com>
2024-04-09 18:31:03 +0800

    Initial Commit

---
+++ SwiftfulSwiftUIinPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulSwiftUIinPractice
+//
+//  Created by Jason on 2024/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulSwiftUIinPractice/SwiftfulSwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIinPracticeApp.swift
+//  SwiftfulSwiftUIinPractice
+//
+//  Created by Jason on 2024/4/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
